4234b01444427deab162e3e1db1c369cc2e5f1bb
1-arua <64558593+1-arua@users.noreply.github.com>
2024-07-23 23:27:46 +0900

    bun の Adapter を使ってみる

--- src/index.tsx
+++ src/index.tsx
@@ -1,90 +1,10 @@
 import { Hono } from "hono";
-import { basicAuth } from "hono/basic-auth";
-import { requestId } from "hono/request-id";
+import { serveStatic } from "hono/bun";
 
 const app = new Hono();
 
-app.get("/", (c) => {
-	return c.text("Hello Hono!");
-});
-
-app.get("/api/hello", (c) => {
-	return c.json({
-		ok: true,
-		message: "Hello Hono!",
-	});
-});
-
-app.get("/posts/:id", (c) => {
-	const page = c.req.query("page");
-	const id = c.req.param("id");
-	c.header("X-Message", "Hi!");
-	return c.text(`You want see ${page} of ${id}`);
-});
-
-app.get("/queries", (c) => {
-	const queries = c.req.queries("p1");
-	return c.text(`You want see ${queries?.toString()}`);
-});
-
-app.get("/routePath/:id", (c) => {
-	return c.text(`You see route path ${c.req.routePath}`);
-});
-
-app.get("/path/:id", (c) => {
-	return c.text(`You see path ${c.req.path}`);
-});
-
-app.get("/very/long/url", (c) => {
-	return c.text(`You see ${c.req.url}`);
-});
-
-app.get("/method", (c) => {
-	return c.text(`Your method is ${c.req.method}\n`);
-});
-
-app.post("/method", (c) => {
-	return c.text(`Your method is ${c.req.method}\n`);
-});
-
-app.delete("/method", (c) => {
-	return c.text(`Your method is ${c.req.method}\n`);
-});
-
-const View = () => {
-	return (
-		<html lang="en">
-			<body>
-				<h1>Hello Hono!</h1>
-			</body>
-		</html>
-	);
-};
-
-app.get("/page", (c) => {
-	return c.html(<View />);
-});
-
-app.get("/htmltext", (c) => {
-	return c.text(<View />);
-});
-
-app.use(
-	"/admin/*",
-	basicAuth({
-		username: "admin",
-		password: "password",
-	}),
-);
-
-app.get("/admin", (c) => {
-	return c.text("Admin page");
-});
-
-app.use("*", requestId());
-
-app.get("/requestId", (c) => {
-	return c.text(`Your request id is ${c.get("requestId")}`);
-});
+app.use("/static/*", serveStatic({ root: "./" }));
+app.get("/", (c) => c.text("You can access: /static/hello.txt"));
+app.get("*", serveStatic({ path: "./static/fallback.txt" }));
 
 export default app;
